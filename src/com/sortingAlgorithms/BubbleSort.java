8421c7e79e10756d5eaa1956acb2a10256f412b3
Charan Raj MS <55379472+CharanRajDeveloper@users.noreply.github.com>
2024-07-17 23:46:59 +0530

    Update BubbleSort.java

--- src/com/sortingAlgorithms/BubbleSort.java
+++ src/com/sortingAlgorithms/BubbleSort.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class BubbleSort {
 	public static int[] bubbleSort(int[] a) {
 		int temp = 0;
+		//Business logic
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a.length - i - 1; j++) {
 				if (a[j] > a[j + 1]) {
